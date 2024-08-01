5761562c718927841d514f589bb61230cf7b4010
kirti <kirti.20cs@saividya.ac.in>
2024-08-01 19:56:27 +0530

    add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
-        <header className="shadow font-nunito ">
-            <div className="max-w-7xl mx-auto flex justify-between">
-                <a href="">Where in the world?</a>
-                <span className="flex items-center">
+        <header className="shadow font-nunito px-6">
+            <div className="max-w-7xl mx-auto flex justify-between py-5">
+                <a href=""><h2 className="font-extrabold text-2xl">Where in the world?</h2></a>
+                <span className="flex items-center gap-2 cursor-pointer font-medium">
                     <i class="fa-regular fa-moon"></i>
                     <p>Dark Mode</p>
                 </span>
