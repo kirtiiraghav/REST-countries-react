ff4511ad2a4383616fa2cc3003f15082974ddb5f
kirti <kirti.20cs@saividya.ac.in>
2024-08-01 19:40:35 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className="shadow font-nunito ">
+            <div className="max-w-7xl mx-auto flex justify-between">
+                <a href="">Where in the world?</a>
+                <span className="flex items-center">
+                    <i class="fa-regular fa-moon"></i>
+                    <p>Dark Mode</p>
+                </span>
+            </div>
+        </header>
+    )
+}
