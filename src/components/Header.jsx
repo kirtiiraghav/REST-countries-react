acaa04794778158faf236ee897a003a2e98f97ee
kirti <kirti.20cs@saividya.ac.in>
2024-08-01 22:39:20 +0530

    make Header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
     return (
-        <header className="shadow font-nunito px-6">
+        <header className="shadow font-nunito px-6 sm:px-12">
             <div className="max-w-7xl mx-auto flex justify-between py-5">
-                <a href=""><h2 className="font-extrabold text-2xl">Where in the world?</h2></a>
-                <span className="flex items-center gap-2 cursor-pointer font-medium">
+                <a href=""><h2 className=" font-bold sm:font-extrabold text-xl sm:text-2xl md:text-3xl">Where in the world?</h2></a>
+                <span className="flex items-center gap-2 cursor-pointer text-xs sm:text-base font-medium ">
                     <i class="fa-regular fa-moon"></i>
-                    <p>Dark Mode</p>
+                    <p className="">Dark Mode</p>
                 </span>
             </div>
         </header>
