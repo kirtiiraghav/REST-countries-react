fe6b81cbe56ee207f3da4502f027ec0541ab2dd1
kirti <kirti.20cs@saividya.ac.in>
2024-08-04 11:12:12 +0530

    dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Header from './components/Header'
 import HomePage from './components/HomePage'
 import { DarkModeProvider } from './contexts/DarkMode'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
     return (
         <DarkModeProvider>
             <Header />
-            <HomePage/>
+            <Outlet />
         </DarkModeProvider>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import './index.css'
+import HomePage from './components/HomePage.jsx';
+import CountryDetails from './components/CountryDetails.jsx';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/:country",
+          element: <CountryDetails />,
+        },
+      ],
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
